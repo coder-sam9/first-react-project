7d6143d7e24f3c83cd141584da533776fa050d3b
coder-sam9 <simplysam679@gmail.com>
2024-12-27 18:53:37 +0530

    new expense item and its styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,14 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem(props) {
   return (
-    <p>
-      <p>Food - $5</p>
-      <p>Petrol - $20</p>
-      <p>Movie - $10</p>
-    </p>
+    <div className="expense-item">
+      <div className="expense-item h2">15 August 2023</div>
+      <div className="expense-item__description">
+      <h2 className="expense-item__description h2 ">Book</h2></div>
+      <div className="expense-item__price">$10</div>
+      <div className="expense-item__location">Delhi</div>
+    </div>
   );
 }
 export default ExpenseItem;
