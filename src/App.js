c5c469637c26de430c3763f7652c47ef6abe2e36
coder-sam9 <simplysam679@gmail.com>
2024-12-27 18:38:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import  ExpenseItem  from "./components/ExpenseItem";
+function App() {
+  return (
+    <div className="App">
+      <h1>
+      Let's get Started
+      </h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(props) {
+  return (
+    <p>
+      <p>Food - $5</p>
+      <p>Petrol - $20</p>
+      <p>Movie - $10</p>
+    </p>
+  );
+}
+export default ExpenseItem;
